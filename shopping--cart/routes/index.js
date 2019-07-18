c6a0071ab848c25480658b514a67b7cec28c9c7e
CrisNar23 <cristian_naranjo_15@hotmail.com>
2019-07-18 13:46:55 -0500

    v-1-0 Avance node.js con conexion a base de datos

--- shopping--cart/routes/index.js
+++ shopping--cart/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var Product = require('../models/product');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+router.get('/', function (req, res, next) {
+  Product.find(function (err, docs) {
+    var productChunks = [];
+    var chunksSize = 3;
+    for (let i = 0; i < docs.length; i += chunksSize) {
+      productChunks.push(docs.slice(i, i + chunksSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+  });
 });
 
 module.exports = router;
