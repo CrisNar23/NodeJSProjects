9ea6b9c953f0369a7886d0b7e064d85df46e8a3a
CrisNar23 <cristian_naranjo_15@hotmail.com>
2020-03-06 20:12:36 -0500

    Avance video 10 al 16

--- udemyCourse/notes-app/app.js
+++ udemyCourse/notes-app/app.js
@@ -1,4 +1,27 @@
-const fs = require('fs');
+//const fs = require('fs')
+//fs.writeFileSync('notes.txt', 'This file was created by Node.js!. It was a new line wrote by CrisNar')
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!. ')
-fs.appendFileSync('notes.txt', 'It was a new line wrote by CrisNar')
+//const validator = require('validator')
+//console.log(validator.isEmail('orangecn23@gmail.com'));
+//console.log(validator.isURL('https://nodejs.org/dist/latest-v12.x/docs/api/fs.html'));
+
+const chalk = require('chalk')
+const getNotes = require('./notes')
+
+const command = process.argv[2]
+
+if (command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove') {
+  console.log('Removing note!');
+}
+
+
+// const notesPrint = getNotes()
+// console.log(notesPrint);
+
+// console.log(chalk.red('"Success!"'));
+
+// const add = require('./utils')
+// const sum = add(4,-2)
+// console.log(sum);
